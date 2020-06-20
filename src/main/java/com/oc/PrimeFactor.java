984fe2b672758b96440e5b799f5ad79d47f6a626
marwa sahraoui <marwasahrawi1988@gmail.com>
2020-06-20 19:18:18 +0200

    first commit

---
+++ src/main/java/com/oc/PrimeFactor.java
@@ -0,0 +1,19 @@
+package com.oc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeFactor {
+
+    public static ArrayList<Integer> CalculatePrimeFactor (int a) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        if(a < 2 ) {
+            return numbers;
+        } else if (a == 2) {
+           
+        }
+
+    }
+}
